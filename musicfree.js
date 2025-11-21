e423b5aee8104b6a792a39985bb928a1fb86b97c
YXYS <51428194+YXYS1024@users.noreply.github.com>
2025-11-21 17:37:31 +0800

    Create musicfree.js

---
+++ musicfree.js
@@ -0,0 +1,39 @@
+{
+  "plugins": [
+    {
+      "name": "wedav",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/webdav.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "哔哩哔哩",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/bilibili.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "小秋音乐",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/qqmusic.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "小易音乐",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/netease.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "小窝音乐",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/kuwo.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "小枸音乐",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/kugou.js",
+      "version": "0.0.1"
+    },
+    {
+      "name": "小咕音乐",
+      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/migu.js",
+      "version": "0.0.1"
+    }
+  ]
+}
