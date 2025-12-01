df7fc52e8de821d0f401da774de34da22e2b2fa9
YXYS <51428194+YXYS1024@users.noreply.github.com>
2025-12-01 09:52:45 +0800

    Update musicfree.js

--- musicfree.js
+++ musicfree.js
@@ -1,39 +1,31 @@
 {
-  "plugins": [
-    {
-      "name": "wedav",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/webdav.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "哔哩哔哩",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/bilibili.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "小秋音乐",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/qqmusic.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "小易音乐",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/netease.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "小窝音乐",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/kuwo.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "小枸音乐",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/kugou.js",
-      "version": "0.0.1"
-    },
-    {
-      "name": "小咕音乐",
-      "url": "https://gitee.com/sinmite/source/raw/master/musicfree/migu.js",
-      "version": "0.0.1"
-    }
-  ]
+    "desc": "0.3.0",
+    "plugins": [
+       
+        {
+            "name": "小易音乐",
+            "url": "http://music.haitangw.net/cqapi/wy.js",
+            "version": "0.1.0"
+        },
+        {
+            "name": "小窝音乐",
+            "url": "http://music.haitangw.net/cqapi/kw.js",
+            "version": "0.1.0"
+        },
+        {
+            "name": "小枸音乐",
+            "url": "http://music.haitangw.net/cqapi/kg.js",
+            "version": "0.1.0"
+        },
+        {
+            "name": "小秋音乐",
+            "url": "http://music.haitangw.net/cqapi/qq.js",
+            "version": "0.1.0"
+        },
+        {
+            "name": "小咕音乐",
+            "url": "http://music.haitangw.net/cqapi/xiaomi.js",
+            "version": "0.1.0"
+        }
+    ]
 }
